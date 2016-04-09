4b8942aa5ee69b6edd7da9701763c7858c99f54c
murphy1312 <murphy@woweed.de>
2016-04-09 19:51:11 +0100

    task 2 & 3 printArray and Sum of Array

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,14 +7,68 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <string>
 
 
 int world_rank;
 
+template<typename Type>
+void printArray(Type *array, int size)
+{
+	for (int i = 0; i<size; i++)
+	{
+		std::cout << array[i] << "\t";
+		
+	}
+	std::cout << std::endl;
+}
+
+template<typename Type>
+Type sum(Type *array, int size)
+{
+	Type sum;
+	for (int i = 0; i<size; i++)
+	{
+		if(i == 0)
+		{
+			sum = array[i];
+		}
+		else
+		{
+			sum = sum + array[i];
+		}
+	}
+	return sum;
+}
+
 /* master node method */
 void coordinator()
 {
-		
+	int arr[] = { 0,4,5,6 };
+	std::string arr2[] = { "hi", "sup" };
+
+	double arr3[] = { 1.3, 4.5, 6.9, 19.12 };
+
+
+	printArray(arr, 4);
+	printArray(arr2, 2);
+	printArray(arr3, 4);
+
+	int summe = 0;
+	summe = sum(arr, 4);
+
+	std::string summe2 = "";
+	summe2 = sum(arr2, 2);
+
+	double summe3 = 0.0;
+	summe3 = sum(arr3, 4);
+
+	std::cout << summe << std::endl;
+	std::cout << summe2 << std::endl;
+	std::cout << summe3 << std::endl;
+
+
+
 }
 
 /* slave node method */
@@ -22,6 +76,8 @@ void participant()
 {
 
 }
+
+
 
 int main(int argc, char** argv) 
 {
@@ -37,10 +93,7 @@ int main(int argc, char** argv)
 	// master node 
 	if(world_rank == 0) 
 	{
-		
 		coordinator();
-		
-
 	}
 	// other nodes
 	else
