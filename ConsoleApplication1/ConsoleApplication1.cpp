285fa749ee70856dab158a74a7b7726bc483feb2
murphy1312 <murphy@woweed.de>
2016-04-09 21:20:54 +0100

    task 5 - scatter partition to each node

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,9 +8,12 @@
 #include <cstdlib>
 #include <cmath>
 #include <string>
+#include <random>
 
 
 int world_rank;
+int world_size;
+int *totalArray;
 
 // print an array
 template<typename Type>
@@ -44,10 +47,6 @@ Type sum(Type *array, int size)
 }
 
 // sumDifferences
-/* takes in a reference to an array, an array size, and
-the overall mean of the dataset. It should produce a sum of the square of differences
-between each value in the dataset and the mean and return this as the result */
-
 template<typename Type>
 Type sumDifferences(Type *array, int size, Type overall_mean)
 {
@@ -67,43 +66,52 @@ Type sumDifferences(Type *array, int size, Type overall_mean)
 	return sumDifference;
 }
 
+// generate random numbers between fMin and fMax
+double fRand(double fMin, double fMax)
+{
+	double f = (double)rand() / RAND_MAX;
+	return fMin + f * (fMax - fMin);
+}
 
 
 /* master node method */
-void coordinator()
+void coordinator(int array_size)
 {
-	int arr[] = {0,4,5,6};
-	std::string arr2[] = { "hi", "sup" };
+	srand(1);
+	totalArray = new int[array_size];
+	for (int i = 0; i < array_size; i++)
+	{
+		totalArray[i] = fRand(0, 50);
+	}
 
-	double arr3[] = { 1.3, 4.5, 6.9, 19.12 };
+	int size_for_each_node = array_size / world_size;
 
-	printArray(arr, 4);
-	printArray(arr2, 2);
-	printArray(arr3, 4);
+	int *partition = new int[size_for_each_node];
 
-	int summe = 0;
-	summe = sum(arr, 4);
+	// broadcast the partition size
+	MPI_Bcast(&size_for_each_node, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	std::string summe2 = "";
-	summe2 = sum(arr2, 2);
-
-	double summe3 = 0.0;
-	summe3 = sum(arr3, 4);
-
-
-	std::cout << summe << std::endl;
-	std::cout << summe2 << std::endl;
-	std::cout << summe3 << std::endl;
+	// scatter the partitions to each node
+	MPI_Scatter(totalArray, size_for_each_node, MPI_INT, partition, size_for_each_node,
+		MPI_INT, 0, MPI_COMM_WORLD);
 
 
-
-
+	// MPI_Reduce()
 
 }
 
 /* slave node method */
 void participant()
 {
+	int size = 0;
+	// receive partition size
+	MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	int *partition = new int[size];
+
+	// receive the partition of the total array this node works on
+	MPI_Scatter(totalArray, size, MPI_INT, partition, size,
+		MPI_INT, 0, MPI_COMM_WORLD);
+	
 
 }
 
@@ -112,18 +120,38 @@ void participant()
 int main(int argc, char** argv) 
 {
 	MPI_Init(NULL, NULL);
-	int iterations = 0;
-	int world_size;
-
 
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+	int array_size = 0;
+	// int *totalArray;
+
+
+	/* get the number of iterations*/
+	sscanf_s(argv[1], "%d", &array_size);
+	// sscanf_s(argv[2], "%d", &totalArray);
+
+	if(array_size == 0)
+	{
+		array_size = 120;
+	}
+
+	if (!(array_size % world_size == 0))
+	{
+		if (world_rank == 0)
+		{
+			std::cout << "Error: array size % world_size-1 != 0" << std::endl;
+		}
+		MPI_Finalize();
+		return 0;
+	}
+
 	
 	// master node 
-	if(world_rank == 0) 
+	if (world_rank == 0)
 	{
-		coordinator();
+		coordinator(array_size);
 	}
 	// other nodes
 	else
@@ -134,6 +162,30 @@ int main(int argc, char** argv)
 	MPI_Finalize();
 	return 0;
 }
+
+/*
+int arr[] = {0,4,5,6};
+std::string arr2[] = { "hi", "sup" };
+
+double arr3[] = { 1.3, 4.5, 6.9, 19.12 };
+
+printArray(arr, 4);
+printArray(arr2, 2);
+printArray(arr3, 4);
+
+int summe = 0;
+summe = sum(arr, 4);
+
+std::string summe2 = "";
+summe2 = sum(arr2, 2);
+
+double summe3 = 0.0;
+summe3 = sum(arr3, 4);
+
+
+std::cout << summe << std::endl;
+std::cout << summe2 << std::endl;
+std::cout << summe3 << std::endl;*/
 
 
 
